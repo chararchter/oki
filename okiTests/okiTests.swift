fd36ababf7ddc3355280fb188494b995d4610c49
vika <chararchter@pm.me>
2025-12-24 12:20:19 +0200

    Initial Commit

---
+++ okiTests/okiTests.swift
@@ -0,0 +1,17 @@
+//
+//  okiTests.swift
+//  okiTests
+//
+//  Created by Viktorija Leimane on 24/12/2025.
+//
+
+import Testing
+@testable import oki
+
+struct okiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
